22c69543eeb02e221798615e2468d7996b039c62
Max Williams <max@new-bamboo.co.uk>
2011-04-26 18:29:47 +0100

    initial commit

---
+++ heroku-sinatra-app.rb
@@ -0,0 +1,43 @@
+# You'll need to require these if you
+# want to develop while running with ruby.
+# The config/rackup.ru requires these as well
+# for it's own reasons.
+#
+# $ ruby heroku-sinatra-app.rb
+#
+require 'rubygems'
+require 'sinatra'
+require 'pusher'
+
+require 'pushercreds'
+
+enable :sessions
+configure(:development) { set :session_secret, "something" }
+
+configure :production do
+  # Configure stuff here you'll want to
+  # only be run at Heroku at boot
+
+  # TIP:  You can get you database information
+  #       from ENV['DATABASE_URI'] (see /env route below)
+end
+
+# Quick test
+get '/' do
+  return File.open("public/index.html")
+end
+
+post '/pusher/auth' do
+  response = Pusher[params[:channel_name]].authenticate(params[:socket_id])
+  return response.to_json
+end
+
+# Test at <appname>.heroku.com
+
+# You can see all your app specific information this way.
+# IMPORTANT! This is a very bad thing to do for a production
+# application with sensitive information
+
+# get '/env' do
+#   ENV.inspect
+# end
